316bfcc63b9030ae73b3a081274b8494d84dbbd3
Schuyler Mortimer <schuymortimer@gmail.com>
2022-11-06 18:25:02 -0800

    add schedule data

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::env;
+
+pub fn read_csv() {
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_path("./data/nba-2022-2023-schedule.csv").unwrap();
+    for result in rdr.records() {
+        print!("{:?}", result.unwrap())
+    }
+    print!("Wow!")
 }
 
 #[cfg(test)]
@@ -7,8 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn reading() {
+        read_csv()
     }
 }
